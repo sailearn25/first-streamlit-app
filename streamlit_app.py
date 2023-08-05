0dee8815c80efc132f8e6654b5be2812d175ad9f
sailearn25 <141470352+sailearn25@users.noreply.github.com>
2023-08-04 23:16:50 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -10,7 +10,7 @@ streamlit.text('sambhar')
 
 import pandas
 my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt")
-my_fruit_list = mu_fruit_list.set_index("Fruit")
+my_fruit_list = my_fruit_list.set_index("Fruit")
 #streamlit.dataframe(my_fruit_list)
 
 streamlit.multiselect("Pick some fruits:", list(my_fruit_list.index))
