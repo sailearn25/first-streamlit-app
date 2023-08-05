0aef349e540078cf6aa0df4d4a12b6b95d840b20
sailearn25 <141470352+sailearn25@users.noreply.github.com>
2023-08-04 23:07:31 -0500

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,3 +6,8 @@ streamlit.header('Breakfast Menu')
 streamlit.text('😀 Idli')
 
 streamlit.text('vada')
+streamlit.text('sambhar')
+
+import pandas
+my_fruit_list = pandas.read_csv("https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_macros.txt')
+streamlit.dataframe(my_fruit_list)
